6589e4308a864ba218a273fc1b9a566834a2b85b
Sree Grandhe <sgrandhe@utexas.edu>
2022-02-21 14:56:35 -0600

    Initial commit for login screen
    Basic UI with login button functionality and handles success and failure

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,12 +1,49 @@
 import React from "react";
+import {Button, Pane} from "evergreen-ui";
+import logo from '../logo.png'
+import {login} from '../utils'
 
 export class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.styles = {
+      container: {
+        width: 300,
+        height: 440,
+        borderRadius: "10px",
+        margin: "10% auto",
+        padding: "4%",
+        boxShadow: "2px 1px 19px -2px #000000",
+      },
+      logo: {
+        width: 150,
+        height: "auto",
+        margin: "0 auto",
+        display: "block",
+        marginBottom: "15%",
+        marginTop: "8%"
+      },
+      button: {
+        margin: "70% 0 auto",
+        display: "block",
+        width: "100%",
+        height: "13%",
+      }
+    }
+  }
+  
+
+
+
     
   render() {
     return (
-      <div>
-        <h1>Login</h1>
-      </div>
+      <Pane style={this.styles.container}>
+        <img src={logo} style={this.styles.logo} />
+        <Button style={this.styles.button}
+        onClick={login}
+        >Log In</Button>
+      </Pane>
     );
   }
 }
