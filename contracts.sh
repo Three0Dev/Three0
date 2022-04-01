1dc92181b750eb68063fcc6b20926a435edd17a9
Sree Grandhe <sgrandhe@utexas.edu>
2022-04-01 18:01:00 -0500

    Add pagination to smart contracts

--- contracts.sh
+++ contracts.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-# npm run prestart
+npm run prestart
 contract=dev-1648778852229-15438908981274
 myaccount=sreegrandhe.testnet 
-# near view $contract get_all_projects '{}'
-# near call $contract create_project '{"name": "new_name", "description": "new_description"}' --accountId $myaccount
-near view $contract get_all_projects '{}'
-near call $contract delete_project '{"project_id": "project_0"}' --accountId $myaccount
-near view $contract get_all_projects '{}'
+# near view $contract get_all_projects '{"offset": 0, "limit": 10}'
+# for i in {1..10}
+# do
+#     near call $contract create_project '{"name": "new_name", "description": "new_description"}' --accountId $myaccount
+# done
+near view $contract get_all_projects '{"offset": 0, "limit": 10}'
+near view $contract get_all_projects '{"offset": 0, "limit": 5}'
+near view $contract get_all_projects '{"offset": 1, "limit": 5}'
+near view $contract get_all_projects '{"offset": 11, "limit": 5}'
