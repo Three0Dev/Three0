604ec7c344c433edd2eb3b9ae391a7a15b621bfb
sparshypop7 <sparshypatel@gmail.com>
2022-04-01 18:02:44 -0500

    pagination on project table

--- src/components/ProjectDisplayTable.jsx
+++ src/components/ProjectDisplayTable.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet, useNavigate, useParams } from "react-router-dom";
 import {Table, DatabaseIcon, UserIcon, Text, Button, PlusIcon, DeleteIcon, EditIcon} from "evergreen-ui"
+import { Pagination } from '@mui/material'
 import { useEffect } from "react";
 // import {useParams, useNavigate} from 'react-router-dom';
 
@@ -8,6 +9,11 @@ export function ProjectDisplayTable(){
     let navigate = useNavigate();
     let [projects, setProjects] = React.useState([]);
 
+    let [page, setPage] = React.useState(1);
+    let updatePage = (e, val) => setPage((val-1)*10);
+
+    
+    
     useEffect(() => {
         async function getProjects(){
             try{
@@ -22,25 +28,26 @@ export function ProjectDisplayTable(){
     }, []);
 
     return (
-        
-        <Table>
-            <Table.Head>
-                <Table.TextHeaderCell><Text>Name</Text></Table.TextHeaderCell>
-                <Table.TextHeaderCell><Text>Description</Text></Table.TextHeaderCell>
-                <Table.TextHeaderCell><Text>Users</Text></Table.TextHeaderCell>
-                <Table.TextHeaderCell><Text>Databases</Text></Table.TextHeaderCell>
-            </Table.Head>
-            <Table.Body>
-            {projects.map((project) => ( 
-                <Table.Row key={project.pid} isSelectable onSelect={() => navigate(`/app/${project.pid}/auth`)}>
-                    <Table.TextCell><Text>{project.name}</Text></Table.TextCell>
-                    <Table.TextCell>{project.description}</Table.TextCell>
-                    <Table.TextCell isNumber>{project.num_users}</Table.TextCell>
-                    <Table.TextCell isNumber>{project.num_databases}</Table.TextCell>
-                </Table.Row>
-            ))}
-            </Table.Body>
-        </Table>
-        
+        <div>
+            <Table>
+                <Table.Head>
+                    <Table.TextHeaderCell><Text>Name</Text></Table.TextHeaderCell>
+                    <Table.TextHeaderCell><Text>Description</Text></Table.TextHeaderCell>
+                    <Table.TextHeaderCell><Text>Users</Text></Table.TextHeaderCell>
+                    <Table.TextHeaderCell><Text>Databases</Text></Table.TextHeaderCell>
+                </Table.Head>
+                <Table.Body>
+                {projects.map((project) => ( 
+                    <Table.Row key={project.pid} isSelectable onSelect={() => navigate(`/app/${project.pid}/auth`)}>
+                        <Table.TextCell><Text>{project.name}</Text></Table.TextCell>
+                        <Table.TextCell>{project.description}</Table.TextCell>
+                        <Table.TextCell isNumber>{project.num_users}</Table.TextCell>
+                        <Table.TextCell isNumber>{project.num_databases}</Table.TextCell>
+                    </Table.Row>
+                ))}
+                </Table.Body>
+            </Table>
+            <Pagination defaultPage={1} count={Math.floor(projects.length/21)+1} boundaryCount={2} onChange={updatePage} variant='outlined'> </Pagination>
+        </div>
     )
 }
