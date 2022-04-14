99522d1bd9c7d8e1b10ffa4a64a1fe9c6750dc95
Sree Grandhe <sgrandhe@utexas.edu>
2022-04-14 13:24:42 -0500

    WIP transaction call without payment

--- src/utils.js
+++ src/utils.js
@@ -22,40 +22,16 @@ export async function initContract() {
   // Getting the Account ID. If still unauthorized, it's just empty string
   window.accountId = window.walletConnection.getAccountId();
 
-
-  // gets the state of the account
-  // const newDevID = `three0d.${window.accountId}`;
-
-  // if(window.accountId) {
-  //   try {
-  //     const account = await near.account(newDevID);
-  //     await account.state();
-  //     window.subaccount = account;
-  //   }catch(e){
-  //     console.log(e);
-  //     // TODO make function access key
-  //     const keyPair = KeyPair.fromRandom("ed25519");
-  //     const publicKey = keyPair.publicKey.toString();
-  //     const keyStore = new keyStores.BrowserLocalStorageKeyStore();
-  //     await keyStore.setKey(nearConfig.networkId, newDevID, keyPair);
-
-  //     window.subaccount = await window.walletConnection.account().createAccount(
-  //       newDevID, // new account name
-  //       publicKey, // public key for new account
-  //       utils.format.parseNearAmount("0.181") // initial balance for new account in yoctoNEAR
-  //     );
-  //   }
-  // }
-
   // Initializing our contract APIs by contract name and configuration
   window.contract = await new Contract(
     window.walletConnection.account(),
     nearConfig.contractName,
     {
+      sender: window.walletConnection.account(),
       viewMethods: ["get_all_projects", "get_project", "get_project_users"],
       changeMethods: ["create_project", "update_project", "delete_project", "add_database", "delete_database"],
-      // // View methods are read only. They don't modify the state, but usually return some value.
-      // // Change methods can modify the state. But you don't receive the returned value when called.
+      // View methods are read only. They don't modify the state, but usually return some value.
+      // Change methods can modify the state. But you don't receive the returned value when called.
     }
   );
 }
