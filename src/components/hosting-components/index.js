914baaf6962cd58d2dc4837c3ca534cb16b6b5fa
Sree Grandhe <sgrandhe@utexas.edu>
2022-10-13 14:03:48 -0500

    Change hosting to custom components

---
+++ src/components/hosting-components/index.js
@@ -0,0 +1,4 @@
+import UploadSystem from './UploadSystem'
+
+// eslint-disable-next-line import/prefer-default-export
+export { UploadSystem }
--- src/views/Hosting.jsx
+++ src/views/Hosting.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import { UploadSystem } from '../components/hosting-components'
 
 export default function Hosting() {
-	return <> </>
+	const [isHostingEnabled, setIsHostingEnabled] = React.useState(false)
+
+	const isHostingEnabledCheck = async () => {
+		setIsHostingEnabled(true)
+	}
+
+	React.useEffect(() => isHostingEnabledCheck(), [])
+
+	return isHostingEnabled ? <UploadSystem /> : <>hi</>
 }
