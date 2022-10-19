35663042f676bdf4a8ae9d3fa523ecf0fea8015d
srawulwar <45615003+srawulwar@users.noreply.github.com>
2022-10-19 18:54:57 -0500

    Create Core.test.tsx

---
+++ src/Core.test.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import { render, fireEvent, screen } from "@testing-library/react";
+import Core from "./Core";
+
+describe("<Core />", () => {
+  test("renders core without crashing", async () => {
+    // render the component
+    // render(<Core />);
+  });
+});
